d02b4eba85428519345b85b9753c6f93ca799efd
shiki <140712093+shiki-01@users.noreply.github.com>
2025-04-11 04:27:01 +0000

    setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import CSSRuntimeProvider from "@master/css.svelte"
+    import config from "../../master.css"
+</script>
+
+<CSSRuntimeProvider config={config}>
+    <main></main>
+</CSSRuntimeProvider>
+
+<style>
+    @import '@master/normal.css';
+</style>
